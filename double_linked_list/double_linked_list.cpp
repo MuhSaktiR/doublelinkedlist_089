07f18abcbf3098b38fe72c8ba4436bd830986bfe
MuhSaktiR <seken876@gmail.com>
2023-05-26 18:12:55 +0700

    memperbaiki dan penambahan

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -14,6 +14,7 @@ class DoubleLinkedList {
 private:
 	Node* START;
 public:
+	DoubleLinkedList();
 	void addNode();
 	bool search(int rollNo, Node** previous, Node** current);
 	bool deleteNode(int rollNo);
